9fe8c34ddbfa48d7aa06b58b20ce0ed58c09dcb8
JOSE SHALU <kjshalu670@gmail.com>
2025-04-24 10:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 image = ""
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image,caption="Uploaded Image", use_container_width=True)
+    st.image(image,caption="Uploaded Image", width=300)
 submit=st.button("Tell me about the Total Calories")
 
 input_prompt = """
